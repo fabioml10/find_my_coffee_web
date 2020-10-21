40a190e72dbb6bcafd0c99a2c4fb7d285c5e58e3
Fabio Muller <fabioml10@yahoo.com.br>
2020-10-21 19:21:51 -0300

    Getting latitude and longitude from browser

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 function App() {
   const { REACT_APP_GOOGLE_API_KEY } = process.env;
+
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+
+  async function setCurrentLocation () {
+    await navigator.geolocation.getCurrentPosition( (position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    }, (error) => alert("Habilite a localização.")
+    )
+  }
+
+  //roda uma única vez
+  useEffect( () => {
+    setCurrentLocation()
+  }, [])
 
   return (
     <>
@@ -11,7 +27,7 @@ function App() {
         <GoogleMap
           mapContainerStyle={{height: "100vh", width: "100vw"}}
           zoom={15}
-          center={{lat: -21.7768, lng: -41.3109}}>
+          center={{lat: latitude, lng: longitude}}>
         </GoogleMap>
       </LoadScript>
     </>
