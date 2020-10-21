9f7c7350e8d7e16931323b6e2ace93c905172a13
Fabio Muller <fabioml10@yahoo.com.br>
2020-10-21 19:39:20 -0300

    Markers created. Images configured

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { useState, useEffect} from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+import GoogleListEstablishmentsService from './services/google_list_establishments'
 
 function App() {
-  const { REACT_APP_GOOGLE_API_KEY } = process.env;
+  const { REACT_APP_GOOGLE_API_KEY } = process.env
 
-  const [latitude, setLatitude] = useState(0);
-  const [longitude, setLongitude] = useState(0);
+  const [latitude, setLatitude] = useState(0)
+  const [longitude, setLongitude] = useState(0)
+  const [locations, setLocations] = useState([])
 
   async function setCurrentLocation () {
     await navigator.geolocation.getCurrentPosition( (position) => {
-      setLatitude(position.coords.latitude);
-      setLongitude(position.coords.longitude);
+      setLatitude(position.coords.latitude)
+      setLongitude(position.coords.longitude)
+      loadCoffeeShops()
     }, (error) => alert("Habilite a localização.")
     )
+  }
+
+  async function loadCoffeeShops() {
+    const response = await GoogleListEstablishmentsService.index(latitude, longitude)
+    setLocations(response.data.results)
   }
 
   //roda uma única vez
@@ -28,6 +37,16 @@ function App() {
           mapContainerStyle={{height: "100vh", width: "100vw"}}
           zoom={15}
           center={{lat: latitude, lng: longitude}}>
+            {
+              locations.forEach((item, index) => {
+                return (
+                  <Marker key={index} icon="/images/coffee-pin.png" title={item.name} animation="4"
+                    position={{lat: item.geometry.location.lat, lng: item.geometry.location.lng}}/>
+                )
+              })
+            }
+            <Marker key="myLocation" icon="/images/my-location-pin.png" title="Meu Local" animation="2"
+              position={{lat: latitude, lng: longitude}}/>
         </GoogleMap>
       </LoadScript>
     </>
