0e75e2ce007762bd4e8ba2bb27e19040aa608147
Fabio Muller <fabioml10@yahoo.com.br>
2020-10-21 19:13:35 -0300

    Map added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 function App() {
-  const { REACT_APP_GOOGLE_API_KEY } = process.env
-  
+  const { REACT_APP_GOOGLE_API_KEY } = process.env;
+
   return (
     <>
       <p>Funcionando</p>
+      <LoadScript googleMapsApiKey={REACT_APP_GOOGLE_API_KEY}>
+        <GoogleMap
+          mapContainerStyle={{height: "100vh", width: "100vw"}}
+          zoom={15}
+          center={{lat: -21.7768, lng: -41.3109}}>
+        </GoogleMap>
+      </LoadScript>
     </>
   );
 }
